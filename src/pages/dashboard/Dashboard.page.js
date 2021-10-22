dc68b8d42e98550a9a4933af682ef28862d06b5a
sailee14 <saileeakim@gmail.com>
2021-10-22 12:49:13 +0530

    Breadcrumb Added

--- src/pages/dashboard/Dashboard.page.js
+++ src/pages/dashboard/Dashboard.page.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { Container,Row,Col, Button } from "react-bootstrap";
+import { Container,Row,Col, Button} from "react-bootstrap";
 import { TicketTable } from "../../components/ticket-table/TicketTable.comp";
+import { PageBreadcrumb } from "../../components/breadcrumb/Breadcrumb.comp";
 import  tickets from '../../assests/data/dummy-tickets.json'
 
 export const Dashboard = () =>{
     return (
         <Container>
+            <Row>
+                <Col>
+                <PageBreadcrumb page="Dashboard"/>
+                </Col>
+            </Row>
             <Row>
                 <Col className="text-center mt-5 mb-2">
                 <Button variant="info" style={{ fontSize:"2rem", padding:"10px 30px"}}>Add New Ticket</Button>
